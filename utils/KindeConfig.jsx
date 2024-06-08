1a45d22957423db8549a031037521f6c194f371e
Dhruv Sharma <dhruvamsh@gmail.com>
2024-06-08 17:44:53 +0530

    Update KindeConfig

--- utils/KindeConfig.jsx
+++ utils/KindeConfig.jsx
@@ -2,6 +2,6 @@ import { KindeSDK } from '@kinde-oss/react-native-sdk-0-7x';
 
 export const client = new KindeSDK(
     'https://dhruvsharma.kinde.com', 
-    'exp://192.168.0.218:8081', 
+    'exp://192.168.43.183:8081',
     '211b34f4bd70463d8e5680b0cc0ccc42', 
-    'exp://192.168.0.218:8081');
+    'exp://192.168.43.183:8081');
